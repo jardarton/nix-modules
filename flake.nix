3cc5276fe046c18210e4618b3c52e55c44064ef0
Jardar <jdr@internal>
2026-06-03 08:56:53 +0200

    feat(flake): support aarch64-linux packages
    Add aarch64-linux to the flake-parts systems list so exported packages, including stack, are available for ARM Linux hosts.

--- flake.nix
+++ flake.nix
@@ -38,7 +38,10 @@
   outputs =
     { flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
       imports = [
         inputs.flake-parts.flakeModules.flakeModules
