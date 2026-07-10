051d2bd4656f8c9910fb35d7e4feb9e7362bdf54
jdr <jdr@internal>
2026-07-10 07:05:28 +0200

    fix(packages): expose cclip only on Linux

--- packages/default.nix
+++ packages/default.nix
@@ -1,11 +1,12 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   packages = {
-    # pkg-caelestia-cli = pkgs.callPackage ./my-caelestia { inherit inputs; };
-    cclip = pkgs.callPackage ./cclip { };
     stack = pkgs.callPackage ./stack { };
     gondolin = pkgs.callPackage ./gondolin { };
     kli = pkgs.callPackage ./kli { };
     playwright-cli = pkgs.callPackage ./playwright-cli { };
+  }
+  // lib.optionalAttrs pkgs.stdenv.isLinux {
+    cclip = pkgs.callPackage ./cclip { };
   };
 }
